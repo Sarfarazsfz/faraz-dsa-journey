0ec4b4cbeb647014fa9ba5a59e829d8fb2ec26c4
Md Sarfaraz Alam <mdsarfarazalam669@gmail.com>
2026-02-25 21:03:48 +0530

    Day 02: Product of two numbers program

---
+++ 02-Java-Basics/UserInput.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class UserInput {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String n = sc.nextLine();
+        System.out.println(n);
+
+        int number = sc.nextInt();
+        System.out.println(number);
+
+        float num = sc.nextFloat();
+        System.out.println(num);
+
+        double price = sc.nextDouble();
+        System.out.println(price);
+        
+    }
+}
