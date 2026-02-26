ce65913df20b61620e5024ae1d98e0f45a3ccdaa
Md Sarfaraz Alam <mdsarfarazalam669@gmail.com>
2026-02-26 23:16:34 +0530

    Day 03: Java input practice

---
+++ 03-Java-Basics/NoIsPositiveOrNegative.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class NoIsPositiveOrNegative {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        // String type = ((number > 0)) ? "Positive" : "Negative";
+        // System.out.println(type);
+        if(number > 0){
+            System.out.println("Entered No is Positive");
+        } else if(number == 0){
+            System.out.println(" Enterd No is Equal");
+        } else{
+            System.out.println("Entered No is Negative");
+        }
+
+    }
+}
